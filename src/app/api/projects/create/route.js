88eb7b774c86b1710ed2f272b53d89f8210fdeaa
satyam kumar <satyarajbarnwal2008@gmail.com>
2026-06-20 00:30:12 +0530

    added a getUser helpr and some api codes for fetching projects

---
+++ src/app/api/projects/create/route.js
@@ -0,0 +1,52 @@
+import { pool } from "@/lib/db";
+import { getUser } from "@/lib/auth";
+
+export async function POST(request) {
+    try {
+
+        const user_id = await getUser()
+        const { name } = await request.json()
+
+        if (!name) {
+            return Response.json(
+                {
+                    success: false,
+                    message: "Project name required"
+                },
+                {
+                    status: 400
+                }
+            );
+        }
+        const result = await pool.query(
+            `INSERT INTO projects
+      (user_id, name)
+      VALUES ($1, $2)
+      RETURNING *`,
+            [user_id, name]
+        );
+
+        return Response.json(
+            {
+                success: true,
+                project: result.rows[0]
+            },
+            {
+                status: 201
+            }
+        );
+
+
+    } catch (error) {
+        console.error(error)
+        return Response.json(
+            {
+                success: false,
+                message: 'server error'
+            },
+            {
+                status: 500
+            }
+        )
+    }
+}
